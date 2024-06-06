b735e293c0e467a2c0c98c5884ad7b5a54a359cb
AveryOn <lnkonion2@gmail.com>
2024-06-06 21:09:29 +0300

    lesson

---
+++ src/components/mainView/profile/profileChunk/editFieldComp.vue
@@ -0,0 +1,44 @@
+<template>
+    <div 
+    :id="`edit-field__${$props.item.id}`"
+    class="edit-field"
+    >
+        <editTextFormComp
+        :item="$props.item"
+        @confirm="(value) => $emit('confirm', value)"
+        @close="$emit('close')"
+        ></editTextFormComp>
+    </div>
+</template>
+
+<script>
+import editTextFormComp from './editTextFormComp.vue';
+export default {
+    components: {
+        editTextFormComp,
+    },
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        }
+    },
+    emits: ['confirm', 'close'],
+}
+</script>
+
+<style scoped>
+.edit-field {
+    position: absolute;
+    right: -100%;
+    display: flex;
+    /* align-self: stretch; */
+    /* justify-content: space-between; */
+    align-items: center;
+    width: 100%;
+    height: 50px;
+    font-size: 1.1rem;
+    background-color: var(--color-bg-main);
+    z-index: 12;
+}
+</style>
---
+++ src/components/mainView/profile/profileChunk/editTextFormComp.vue
@@ -0,0 +1,125 @@
+<template>
+    <form 
+    class="edit-text-form"
+    @submit.prevent>
+        <input type="text"
+        v-model="textInput"
+        class="edit-text-form__input"
+        :placeholder="`${$props.item.key}`"
+        >
+
+        <!-- CONFIRM -->
+        <button 
+        class="edit-text-form__btn confirm"
+        @click="confirm"
+        >
+            <font-awesome-icon
+            class="icon"
+            icon="fa-check"
+            ></font-awesome-icon> 
+        </button>
+
+        <!-- RESET -->
+        <button 
+        class="edit-text-form__btn reset"
+        @click="reset"
+        >
+            <font-awesome-icon
+            class="icon"
+            icon="fa-rotate-left"
+            ></font-awesome-icon> 
+        </button>
+
+        <!-- CLOSE -->   
+        <button 
+        class="edit-text-form__btn close"
+        @click="close"
+        >
+            <font-awesome-icon
+            class="icon"
+            icon="fa-xmark"
+            ></font-awesome-icon> 
+        </button>
+    </form> 
+</template>
+
+<script>
+import { animationFadeText } from '@/utils/animationUtils';
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        }
+    },
+    emits: [ 'confirm', 'close', ],
+    data() {
+        return {
+            textInput: '',
+        }
+    },
+    methods: {
+        confirm() {
+            this.$emit('confirm', this.textInput);
+            this.textInput = '';
+        },
+        reset() {
+            animationFadeText(this.textInput, (result) => {
+                this.textInput = result;
+            });
+        },
+        close() {
+            this.$emit('close');
+        },
+    }
+}
+</script>
+
+<style scoped>
+.edit-text-form {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    align-self: stretch;
+}
+.edit-text-form__input {
+    width: 75%;
+    height: 100%;
+    font-family: var(--font);
+    background-color: var(--color-bg-input);
+    font-size: medium;
+    padding: 0 1.5rem;
+    outline: rgba(0,0,0,0);
+}
+.edit-text-form__btn {
+    width: 8%;
+    height: 100%;
+    background-color: var(--color-bg-input);
+    border: none;
+    outline: rgba(0,0,0,0);
+    cursor: pointer;
+    font-size: 1.1rem;
+    transition: all .6s ease;
+}
+.edit-text-form__btn.confirm {
+    color: green;
+}
+.edit-text-form__btn.reset {
+    color: orange;
+}
+.edit-text-form__btn.close {
+    color: red;
+}
+
+.edit-text-form__btn:hover {
+    transition: all .6s ease;
+    background-color: rgba(128, 128, 128, 0.35);
+}
+
+.edit-text-form__btn:hover .icon {
+    transition: all .3s ease;
+    transform: rotate(-360deg);
+}
+</style>
