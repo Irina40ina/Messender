31259ebf3f302e952fa74f08fdf46acc493d6dc2
Irina40ina <kanishevairina@mail.ru>
2024-06-23 19:00:40 +0300

    vv

---
+++ src/components/mainView/messanger/messangerChatsComp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="messanger-container__chats">
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .messanger-container__chats {
+        width: 20%;
+        
+
+    }
+</style>
--- src/components/mainView/messanger/messangerComp.vue
+++ src/components/mainView/messanger/messangerComp.vue
@@ -1,15 +1,27 @@
 <template>
     <div class="messanger-container">
-        MESSANGER COMP
+        <messangerChatsComp></messangerChatsComp>
+        <messangerWidgetComp></messangerWidgetComp>
+        
     </div>
 </template>
 
 <script>
+import messangerChatsComp from "@/components/mainView/messanger/messangerChatsComp.vue"
+import messangerWidgetComp from "@/components/mainView/messanger/messangerWidgetComp.vue"
+
 export default {
-    
+    components: {
+        messangerChatsComp,
+        messangerWidgetComp,
+    }
 }
 </script>
 
 <style scoped>
-    
+    .messanger-container {
+        width: 100%;
+        height: 100%;
+        display: flex;
+    }
 </style>
